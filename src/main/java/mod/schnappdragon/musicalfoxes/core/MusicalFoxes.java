954dd45e2d17c02e742844b8994846eef3329ebf
SnappyDragon64 <pg61151@gmail.com>
2024-06-18 14:01:58 +0530

    1.21 port

--- src/main/java/mod/schnappdragon/musicalfoxes/core/MusicalFoxes.java
+++ src/main/java/mod/schnappdragon/musicalfoxes/core/MusicalFoxes.java
@@ -1,11 +1,13 @@
 package mod.schnappdragon.musicalfoxes.core;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(MusicalFoxes.MODID)
 public class MusicalFoxes {
     public static final String MODID = "musicalfoxes";
 
-    public MusicalFoxes() {
+    public MusicalFoxes(IEventBus modEventBus, ModContainer modContainer) {
     }
 }
